6c15d3acb1edf29e7d3639aa343f09cf0afb841c
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-05-15 21:50:20 -0400

    adding icons

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Osca, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion</span>
+        <div className="Succes-map">Google maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
